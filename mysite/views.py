cb4eff0b42967c756b0452877f60a1a1ab8d57b5
CJCU-CC <108b01165@mailst.cjcu.edu.tw>
2021-04-06 11:45:26 +0800

    0406

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,10 @@
-def index(request):
-    # posts = models.Post.objects.filter(enabled=True).order_by('-pub_time')[:30]
-    posts = models.Post.objects.all().order_by('-pub_time')
+from django.shortcuts import render
+from django.http import HttpResponse
+from mysite import models
+
+
+def index(request, pid=None, del_pass=None):
+    posts = models.Post.objects.filter(enabled=True).order_by('-pub_time')[:30]
     moods = models.Mood.objects.all()
     try:
         user_id = request.GET['user_id']
@@ -9,12 +13,23 @@ def index(request):
         user_mood = request.GET['mood']
     except:
         user_id = None
-        message = '如果要張貼訊息，則每一個欄位都要填寫'
+        message = '如要張貼訊息，則每一個欄位都要填...'
 
-    if user_id is not None:
+    if del_pass and pid:
+        try:
+            post = models.Post.objects.get(id=pid)
+        except:
+            post = None
+        if post:
+            if post.del_pass == del_pass:
+                post.delete()
+                message = "資料刪除成功"
+            else:
+                message = "密碼錯誤"
+    elif user_id != None:
         mood = models.Mood.objects.get(status=user_mood)
         post = models.Post.objects.create(mood=mood, nickname=user_id, del_pass=user_pass, message=user_post)
         post.save()
-        message = '成功儲存! 請記得你的編輯密碼[{}]!'.format(user_pass)
+        message = '成功儲存！請記得你的編輯密碼[{}]!，訊息需經審查後才會顯示。'.format(user_pass)
 
     return render(request, 'index.html', locals())
